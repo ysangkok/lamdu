dfa7919bec7cd62f5f649130db74646441773af5
Janus <ysangkok@gmail.com>
2017-02-19 04:16:54 +0100

    update to new Definition based stuff

--- Janus.hs
+++ Janus.hs
@@ -18,15 +18,15 @@ import qualified Lamdu.Calc.Val as Val
 import qualified Lamdu.Calc.Type
 import Lamdu.Calc.Identifier (Identifier(..))
 import Data.ByteString.Char8 (pack)
+import Lamdu.Data.Definition (Expr(..), Body(..), expr)
+import qualified Lamdu.Data.Definition as Def
+import Control.Lens.Review (review)
 
 withDb :: MVar (Maybe Db) -> (Db -> IO a) -> IO a
 withDb mvar action =
     withMVar mvar $ \case
     Nothing -> error "Trying to use DB when it is already gone"
     Just db -> action db
-
-replIRef :: IRef ViewM (ExprIRef.ValI ViewM)
-replIRef = DbLayout.repl DbLayout.codeIRefs
 
 runViewTransactionInIO :: MVar (Maybe Db) -> Transaction ViewM a -> IO a
 runViewTransactionInIO dbM trans =
@@ -48,18 +48,16 @@ doFun readRef compiled = do
 main = do
         withDB "/home/janus/.lamdu" $ \db -> do
           dbmvar <- newMVar (Just db)
-          let x = Transaction.readIRef (DbLayout.repl DbLayout.codeIRefs) >>= ExprIRef.readVal
+          let x = Transaction.readIRef (DbLayout.repl DbLayout.codeIRefs) >>= traverse ExprIRef.readVal
           readRef <- runViewTransactionInIO dbmvar x
           compiled <- runViewTransactionInIO dbmvar ( compile readRef )
-          (val, val2) <- doFun readRef compiled
+          let ex = view expr readRef :: Annotated.Val (ExprIRef.ValI ViewM)
+          (val, val2) <- doFun ex compiled
           let x = (Lamdu.Calc.Type.NominalId $ Identifier $ pack "BI:float\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL" , pack "\SOH\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\a\NUL\224X=\177|\DC1\255\255\255\255\255\255\255\243")
           let y = Val.LLiteral (Val.PrimVal (fst x) (snd x))
-          let z2 = y == val
-          -- let z = (Annotated.Val (Val.BLeaf y)) == val2
+          let z = y == val
           case val of
             Val.LLiteral (Val.PrimVal p d) -> do
               print $ (p, d)
               print $ z
             _ -> error "lol"
-          -- print res
-          -- print compiled
